af689dde465ac79634a56f3797c446e68e286f6b
yuhsin.chen <s63232@gmail.com>
2024-01-20 01:29:23 +0900

    Add copy-text-to-clipboard and marked as dependencies

--- src/popup/Popup.vue
+++ src/popup/Popup.vue
@@ -1,9 +1,17 @@
 <script setup lang="ts">
+import { marked } from 'marked'
 import { storageDemo } from '~/logic/storage'
 
 function openOptionsPage() {
   browser.runtime.openOptionsPage()
 }
+
+const htmlOfCopied = ref('')
+browser.tabs.query({
+  active: true,
+  currentWindow: true,
+}).then(tabs => marked.parse(`[${tabs[0].title}](${tabs[0].url})`))
+  .then(html => htmlOfCopied.value = html)
 </script>
 
 <template>
@@ -12,6 +20,7 @@ function openOptionsPage() {
     <div>Popup</div>
     <SharedSubtitle />
 
+    <div v-html="htmlOfCopied" />
     <button class="btn mt-2" @click="openOptionsPage">
       Open Options
     </button>
