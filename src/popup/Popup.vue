82b21363d637db609c111679cf3b744d3d3e734f
cyx <cyx@chen-yu-hsin.me>
2024-02-23 14:48:45 +0900

    Add EpSuccessFilled icon to Popup component

--- src/popup/Popup.vue
+++ src/popup/Popup.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import { marked } from 'marked'
 import { storageDemo } from '~/logic/storage'
+import EpSuccessFilled from '~icons/ep/success-filled'
 
 function openOptionsPage() {
   browser.runtime.openOptionsPage()
@@ -17,8 +18,7 @@ browser.tabs.query({
 
 <template>
   <main class="w-[300px] px-4 py-5 text-center text-gray-700">
-    <Logo />
-    <div>Popup</div>
+    <EpSuccessFilled class="text-green-700 text-4xl mx-auto" />
     <SharedSubtitle />
 
     <div v-html="htmlOfCopied" />
