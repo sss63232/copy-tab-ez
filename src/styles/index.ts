7cb8b5e7fa953445a4a4499474d7e0dfa01ba75d
cyx <cyx@chen-yu-hsin.me>
2024-02-26 00:29:25 +0900

    Add shared reset CSS and link to unocss/reset package

--- src/styles/index.ts
+++ src/styles/index.ts
@@ -1,3 +1,7 @@
+// this is a shared reset CSS for popup and options page
+
+// @see [@unocss/reset - npm](https://www.npmjs.com/package/@unocss/reset?activeTab=code)
 import '@unocss/reset/tailwind.css'
+
 import './main.css'
 import 'uno.css'
